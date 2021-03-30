8bf6bbb48b1db256bcff4c2ecfc91401e48281af
jummi <98qkrwjdal@naver.com>
2021-03-31 05:11:12 +0900

    test: 당첨 확인 기능 구현 테스트 작성

---
+++ src/main/java/domain/Result.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class Result {
+    private final List<Integer> result;
+
+    public Result(List<Integer> result) {
+        this.result = result;
+    }
+
+    public List<Integer> getResult() {
+        return result;
+    }
+}
---
+++ src/test/java/domain/LotteryMachineTest.java
@@ -0,0 +1,44 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LotteryMachineTest {
+
+    private LotteryMachine lotteryMachine;
+    private int purchasedCount = 6;
+    private Lotteries lotteries = new Lotteries(Arrays.asList(
+        new Lottery(new Numbers(Arrays.asList(8, 2, 23, 41, 4, 5))),
+        new Lottery(new Numbers(Arrays.asList(3, 5, 29, 6, 2, 38))),
+        new Lottery(new Numbers(Arrays.asList(4, 31, 5, 40, 2, 1))),
+        new Lottery(new Numbers(Arrays.asList(4, 1, 3, 45, 5, 2))),
+        new Lottery(new Numbers(Arrays.asList(7, 1, 2, 3, 4, 5))),
+        new Lottery(new Numbers(Arrays.asList(1, 2, 3, 4, 5, 6)))
+    ));
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void 당첨_번호를_확인한다() throws Exception {
+        //given
+        String winningNumbers = "1, 2, 3, 4, 5, 6";
+        int bonusNumber = 7;
+        lotteryMachine = new LotteryMachine(winningNumbers, bonusNumber);
+
+        //when
+        Result result = lotteryMachine.compareNumbers(lotteries, purchasedCount);
+
+        //then
+        assertThat(result.getResult().get(0)).isEqualTo(1);
+        assertThat(result.getResult().get(1)).isEqualTo(2);
+        assertThat(result.getResult().get(2)).isEqualTo(1);
+        assertThat(result.getResult().get(3)).isEqualTo(1);
+        assertThat(result.getResult().get(4)).isEqualTo(1);
+    }
+}
