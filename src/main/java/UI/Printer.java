1318d7aea2a9cfb7617e6b3427471264c1182a65
Chae hee chan <eqholic1125@gmail.com>
2021-04-02 04:33:56 +0900

    feat: 로또 번호 출력 기능 추가

--- src/main/java/UI/Printer.java
+++ src/main/java/UI/Printer.java
@@ -1,9 +1,21 @@
 package UI;
 
+import Domain.Lotto;
+import Domain.Lottos;
+
 public class Printer {
     private static final String PRICE_REQUEST_MESSAGE = ("구입금액을 입력해 주세요.");
+    private static final String TICKET_COUNT_MESSAGE = ("개를 구매했습니다.");
 
     public void requestPrice(){
         System.out.println(PRICE_REQUEST_MESSAGE);
+    }
+
+    public void printTicketCount(int count){
+        System.out.println(count + TICKET_COUNT_MESSAGE);
+    }
+
+    public void printLottoNumber(Lotto lotto){
+        System.out.println(lotto.getLottoNumber());
     }
 }
