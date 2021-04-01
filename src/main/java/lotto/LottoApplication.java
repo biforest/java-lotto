114b9291f671ab1139b83127320af13d0fa8f1c5
Sehoon In <dlstpgns0406@gmail.com>
2021-04-02 08:45:02 +0900

    refactor: run() method 구조 변경 및 makeLottos method 분리

--- src/main/java/lotto/LottoApplication.java
+++ src/main/java/lotto/LottoApplication.java
@@ -7,43 +7,39 @@ import lotto.domain.lottoticket.LottoTicket;
 import lotto.domain.lottoticket.Lottos;
 import lotto.domain.lottoticket.NumberOfLottoTicket;
 import lotto.domain.strategy.RandomLottoNumberStrategy;
+import lotto.ui.Printer;
+import lotto.ui.Receiver;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class LottoApplication {
-    Scanner scanner = new Scanner(System.in);
-    private static RandomLottoNumberStrategy randomLottoNumberStrategy = new RandomLottoNumberStrategy();
+    private final Printer printer = new Printer();
+    private final Receiver receiver = new Receiver();
     private static LottoMachine lottoMachine = new LottoMachine();
-    public void run(){
+    private final RandomLottoNumberStrategy randomLottoNumberStrategy = new RandomLottoNumberStrategy();
+
+    public void run() {
+        printer.requestPurchaseAmount();
+        NumberOfLottoTicket numberOfLottoTicket = new NumberOfLottoTicket(receiver.receiveLottoTotalAmount());
+        Lottos lottos = makeLottos(numberOfLottoTicket);
+        printer.printAllLotto(lottos);
+        printer.requestLastWeekLottoWinningNumber();
+        List<Integer> LastWeekLottoWinningNumbers = receiver.receiveLastWeekLottoWinningNumbers();
+        LastWeekWinningLotto lastWeekWinningLotto = new LastWeekWinningLotto(LastWeekLottoWinningNumbers);
+        printer.requestLottoBonusBallNumber();
+        LastWeekWinningBonusBall lastWeekWinningBonusBall = new LastWeekWinningBonusBall(receiver.receiveLottoBonusBallNumber());
+        lottoMachine.Discriminator(lottos, lastWeekWinningLotto, lastWeekWinningBonusBall, numberOfLottoTicket);
+    }
+
+    private Lottos makeLottos(NumberOfLottoTicket numberOfLottoTicket) {
         ArrayList<LottoTicket> lottoDummy = new ArrayList<>();
-        System.out.println("구입금액을 입력해 주세요.");
-        int money = scanner.nextInt();
-        NumberOfLottoTicket numberOfLottoTicket = new NumberOfLottoTicket(money);
-        System.out.println(numberOfLottoTicket.getNumberOfLottoTicket());
         for (int i = 0; i < numberOfLottoTicket.getNumberOfLottoTicket(); i++) {
             LottoTicket lotto = new LottoTicket(randomLottoNumberStrategy.getRandomLottoNumbers());
             lottoDummy.add(lotto);
         }
         Lottos lottos = new Lottos(lottoDummy);
-        for (LottoTicket lotto : lottos.getLottos())
-        {
-            System.out.println(lotto.getLotto());
-        }
-        System.out.println("지난 주 당첨번호를 적어주세요");
-        String winningLottoNumbers = scanner.next();
-        List<String> winningLotto = Arrays.asList(winningLottoNumbers.split(","));
-        List<Integer> winningLottoNumber = winningLotto.stream().map(Integer::parseInt).collect(Collectors.toList());
-
-        LastWeekWinningLotto lastWeekWinningLotto = new LastWeekWinningLotto(winningLottoNumber);
-
-        System.out.println("보너스 볼을 입력해주세요");
-        int bonusBall = scanner.nextInt();
-        LastWeekWinningBonusBall lastWeekWinningBonusBall = new LastWeekWinningBonusBall(bonusBall);
-        lottoMachine.Discriminator(lottos, lastWeekWinningLotto, lastWeekWinningBonusBall, numberOfLottoTicket);
+        return lottos;
     }
 
     public static void main(String[] args) {
