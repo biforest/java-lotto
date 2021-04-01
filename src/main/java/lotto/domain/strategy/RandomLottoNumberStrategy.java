a6ef619f8c09b33b596f910a7bfe09c96e6e5810
Sehoon In <dlstpgns0406@gmail.com>
2021-04-01 23:45:07 +0900

    feat: RandomLottoNumberStrategy class 생성 및 shuffle을 통해서 중복되지 않는 랜덤숫자 6개를 리스트로 반환하는 method 구현

--- src/main/java/lotto/domain/strategy/RandomLottoNumberStrategy.java
+++ src/main/java/lotto/domain/strategy/RandomLottoNumberStrategy.java
@@ -1,11 +1,31 @@
 package lotto.domain.strategy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class RandomLottoNumberStrategy {
-    private final static int UPPER_BOUND = 46;
+    private final static int UPPER_BOUND = 45;
+    private final static int LOWER_BOUND = 1;
+    private final static int LOTTO_NUMBER_COUNT = 6;
 
-    public int decideNumber(){
-        return new Random().nextInt(UPPER_BOUND);
+    public List<Integer> getRandomLottoNumbers(){
+        List<Integer> lottoNumberRange = setLottoNumberRange();
+        return getLottoNumbers(lottoNumberRange);
     }
+
+    private List<Integer> setLottoNumberRange(){
+        ArrayList<Integer> lottoNumberIndex = new ArrayList<>();
+        for (int lottoNumber = LOWER_BOUND; lottoNumber < UPPER_BOUND; lottoNumber++) {
+            lottoNumberIndex.add(lottoNumber);
+        }
+        return lottoNumberIndex;
+    }
+
+    private List<Integer> getLottoNumbers(List<Integer> lottoNumberRange){
+        Collections.shuffle(lottoNumberRange);
+        return lottoNumberRange.subList(0,LOTTO_NUMBER_COUNT);
+    }
+
 }
