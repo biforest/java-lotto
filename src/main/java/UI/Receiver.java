3406e4a6fec07ee947182b7f3c3027d87fdc45f1
Chae hee chan <eqholic1125@gmail.com>
2021-04-01 19:39:03 +0900

    feat: 입력 타입 String으로 변경

--- src/main/java/UI/Receiver.java
+++ src/main/java/UI/Receiver.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Receiver {
     Scanner scanner = new Scanner(System.in);
 
-    public Integer receiveInt(){
-        return scanner.nextInt();
+    public String receiveLine(){
+        return scanner.nextLine();
     }
 }
