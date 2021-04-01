7c5412b2c7bed456387b60bf158a7bff345f4be3
Sehoon In <dlstpgns0406@gmail.com>
2021-04-02 06:54:27 +0900

    feat: 맞춘 로또 티켓의 당첨금액을 리스트로 만드는 WinningLottoTicketPrices method 구현

--- src/main/java/lotto/domain/EnumWinningStatus.java
+++ src/main/java/lotto/domain/EnumWinningStatus.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.domain.lottoticket.NumberOfLottoTicket;
 import lotto.domain.lottowinningresult.LottoWinningBonusBallResult;
 import lotto.domain.lottowinningresult.LottoWinningResult;
 
@@ -7,22 +8,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EnumWinningStatus {
-    private List<LottoFactory> lottoFactories;
-
-    public List<Long> mappingMatchedLottoWithWinningMoney(LottoFactory lottoWinningResult){
+    private ArrayList<Integer> lottoPrices = new ArrayList<>();
+    public List<Long> mappingMatchedLottoWithWinningMoney(LottoFactory lottoFactory, NumberOfLottoTicket numberOfLottoTicket){
         ArrayList<Long> mappingMatchedLottoWithWinningMoney = new ArrayList<>();
+        LottoWinningResult lottoWinningResults = lottoFactory.getLottoWinningResult();
+        LottoWinningBonusBallResult lottoWinningBonusBallResult = lottoFactory.getLottoWinningBonusBallResult();
 
-        for(WinningStatus winningStatus: WinningStatus.values()){
-            long lottoWinningResultsCount = lottoFactories.stream().
-                    filter(lottoMatchCount -> lottoMatchCount.equals(winningStatus.getMatchCount())).
-                    filter(lottoMatchBonusBall -> (lottoMatchBonusBall.equals(winningStatus.hasBonusBall()))).
-                    count();
-            mappingMatchedLottoWithWinningMoney.add(lottoWinningResultsCount);
-            System.out.println("----------------------");
-            System.out.println(winningStatus.getMatchCount());
-            System.out.println(lottoWinningResultsCount);
+        for (int i = 0; i < numberOfLottoTicket.getNumberOfLottoTicket(); i++) {
+            int lottoMatchedNumber = lottoWinningResults.getLottoWinningResult().get(i);
+            Boolean lottoBonusBallNumber = lottoWinningBonusBallResult.getLottoWinningBonusBallResult().get(i);
+            getWinningLottoTicketPrices(lottoMatchedNumber, lottoBonusBallNumber);
         }
+        System.out.println(lottoPrices);
         return mappingMatchedLottoWithWinningMoney;
+    }
+
+    private void getWinningLottoTicketPrices(int lottoMatchedNumber, boolean lottoBonusBallNumber) {
+        for(WinningStatus winningStatus: WinningStatus.values()){
+            int matchedWinningNumberCount = winningStatus.getMatchCount();
+            boolean isMatchedBonusBallCount = winningStatus.hasBonusBall();
+            makeWinningLottoTicket(lottoMatchedNumber, matchedWinningNumberCount, isMatchedBonusBallCount, lottoBonusBallNumber, winningStatus);
+        }
+    }
+
+    private void makeWinningLottoTicket(
+            int lottoMatchedNumber,
+            int matchedWinningNumberCount,
+            boolean isMatchedBonusBallCount,
+            boolean lottoBonusBallNumber,
+            WinningStatus winningStatus
+    ){
+        if((lottoMatchedNumber == matchedWinningNumberCount) && (isMatchedBonusBallCount == lottoBonusBallNumber)){
+            lottoPrices.add(winningStatus.getWinningMoney());
+        }
+
     }
 
 }
