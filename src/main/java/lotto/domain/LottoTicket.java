b1e38cbf836858d2ef3d7794a2635c106c91c6e8
pkalsh <pkalsh345@gmail.com>
2021-04-02 12:06:42 +0900

    feat: 로또 발행시 생성되는 LottoTicket 객체 구현
    - 한 티켓당 로또 번호가 몇 개인지에 관한 책임
    - LottoNumber VO의 콜렉션을 래핑한 객체

---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoTicket {
+    private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
+
+    private final List<LottoNumber> lottoNumbers;
+
+    public LottoTicket() {
+        List<LottoNumber> lottoNumbers = LottoNumber.range();
+        lottoNumbers = generateLottoNumbers(lottoNumbers);
+        this.lottoNumbers = new ArrayList<>(lottoNumbers);
+    }
+
+    private List<LottoNumber> generateLottoNumbers(List<LottoNumber> lottoNumbers) {
+        Collections.shuffle(lottoNumbers);
+        lottoNumbers = lottoNumbers.stream()
+                .limit(NUMBER_OF_LOTTO_NUMBERS)
+                .sorted(Comparator.comparing(LottoNumber::getLottoNumber))
+                .collect(Collectors.toList());
+
+        return lottoNumbers;
+    }
+
+}
