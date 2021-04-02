fce032a38f890207ec38409d464c53ca9b718a65
pkalsh <pkalsh345@gmail.com>
2021-04-02 16:18:17 +0900

    test: 보너스 번호가 당첨 번호에 포함되는 입력이면 에외 발생하는 테스트 작성

---
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinningNumberTest {
+
+    @DisplayName("보너스 번호는 당첨 번호에 포함되는 숫자여서는 안된다.")
+    @Test
+    void notWinningNumberContainsBonus() {
+        //given
+        List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 10, 30);
+        int bonusNumber = 10;
+
+        //when then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new WinningNumber(lottoNumbers, bonusNumber))
+                .withMessage("보너스 번호는 당첨 번호와 다른 번호여야 합니다.");
+    }
+
+}
