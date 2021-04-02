2b89a23472b73ece4ffacab421580306d78c5be7
Sehoon In <dlstpgns0406@gmail.com>
2021-04-02 11:11:36 +0900

    feat: printAllMatchedLottoResult method 구현 및 printLottoProfit method 구현

--- src/main/java/lotto/ui/Printer.java
+++ src/main/java/lotto/ui/Printer.java
@@ -1,13 +1,17 @@
 package lotto.ui;
 
+import lotto.domain.WinningStatus;
 import lotto.domain.lottoticket.LottoTicket;
 import lotto.domain.lottoticket.Lottos;
+
+import java.util.*;
 
 public class Printer {
     private static final String REQUEST_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String REQUEST_LAST_WEEK_LOTTO_WINNING_NUMBER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     private static final String REQUEST_LOTTO_BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해주세요";
-
+    private static final String PRINT_FINAL_MATCHED_LOTTO_RESULT_MESSAGE = "%s개 일치 (%s원)- %s개";
+    private static final String PRINT_LOTTO_PROFIT_MESSAGE = "총 수익률은 %f입니다.";
     public void requestPurchaseAmount() {
         System.out.println(REQUEST_PURCHASE_AMOUNT_MESSAGE);
     }
@@ -21,6 +25,21 @@ public class Printer {
         for (LottoTicket lotto : lottos.getLottos()) {
             System.out.println(lotto.getLotto());
         }
+    }
+
+    public void printAllMatchedLottoResult(Map<Integer, Integer> lottoPrices) {
+        List<Integer> keySet = new ArrayList(lottoPrices.keySet());
+        keySet.sort(Comparator.comparingInt(lottoPrices::get));
+
+        for(Integer key: keySet) {
+//            WinningStatus lottoMatchCountKey = getMappingMatchedLottoCount(key);
+            int lottoMatchCountKey = 1;
+            System.out.println(String.format(PRINT_FINAL_MATCHED_LOTTO_RESULT_MESSAGE, lottoMatchCountKey, key, lottoPrices.get(key)));
+        }
+    }
+
+    public void printLottoProfit(float calculatedProfit) {
+        System.out.println(String.format(PRINT_LOTTO_PROFIT_MESSAGE, calculatedProfit));
     }
 }
 
