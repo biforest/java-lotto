8573f206705392298d870d21da7550ad719a0de1
Sehoon In <dlstpgns0406@gmail.com>
2021-04-02 10:40:17 +0900

    feat: NumberOfLottoTicket class 안에 getMoney method 구현

--- src/main/java/lotto/domain/lottoticket/NumberOfLottoTicket.java
+++ src/main/java/lotto/domain/lottoticket/NumberOfLottoTicket.java
@@ -3,9 +3,11 @@ package lotto.domain.lottoticket;
 public class NumberOfLottoTicket {
     private final static int LOTTO_PRICE = 1000;
     private final int tickets;
+    private final int money;
 
-    public NumberOfLottoTicket(int money){
+    public NumberOfLottoTicket(int money) {
         this.tickets = setNumberOfLottoTickets(money);
+        this.money = money;
     }
 
     //    naming 바꾸기
@@ -13,7 +15,11 @@ public class NumberOfLottoTicket {
         return money / LOTTO_PRICE;
     }
 
-    public int getNumberOfLottoTicket(){
+    public int getNumberOfLottoTicket() {
         return this.tickets;
+    }
+
+    public int getMoney() {
+        return this.money;
     }
 }
