40543c850fb880a4aafba7a98d1505ad69d739e5
Chae hee chan <eqholic1125@gmail.com>
2021-04-02 23:46:36 +0900

    feat: LottoApplication 구현

---
+++ src/main/java/lotto/LottoApplication.java
@@ -0,0 +1,46 @@
+package lotto;
+import lotto.domain.*;
+import lotto.ui.Printer;
+import lotto.ui.Receiver;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+public class LottoApplication {
+    private static final String WINNING_NUMBER_INPUT_DELIMITER = ", ";
+    private final Printer printer;
+    private final Receiver receiver;
+    public LottoApplication(Printer printer, Receiver receiver) {
+        this.printer = printer;
+        this.receiver = receiver;
+    }
+    public void run() {
+        int purchaseAmount = receivePurchaseAmount();
+        LottoTickets lottoTickets = Issuer.issueLottoTickets(purchaseAmount);
+        printer.printIssuedTickets(lottoTickets);
+        WinningNumber winningNumber = new WinningNumber(receiveWinningNumber(), receiveBonusNumber());
+        GameResults gameResults = lottoTickets.matchNumbers(winningNumber);
+        Statistics statistics = new Statistics(gameResults, purchaseAmount);
+        printer.printStatistics(statistics);
+    }
+    private int receivePurchaseAmount() {
+        printer.requestPurchaseAmount();
+        String purchaseAmount = receiver.receiveLine();
+        return Integer.parseInt(purchaseAmount);
+    }
+    private List<Integer> receiveWinningNumber() {
+        printer.requestWinningNumber();
+        String winningNumber = receiver.receiveLine();
+        return Arrays.stream(winningNumber.split(WINNING_NUMBER_INPUT_DELIMITER))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
+    private int receiveBonusNumber() {
+        printer.requestBonusNumber();
+        String bonusNumber = receiver.receiveLine();
+        return Integer.parseInt(bonusNumber);
+    }
+    public static void main(String[] args) {
+        LottoApplication app = new LottoApplication(new Printer(), new Receiver());
+        app.run();
+    }
+}
