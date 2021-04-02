1e16e0db6233841b9d8a3822de840aecfa4aa424
Chae hee chan <eqholic1125@gmail.com>
2021-04-02 23:52:44 +0900

    feat: WinningResult 구현

---
+++ src/main/java/lotto/domain/WinningResult.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+public class WinningResult {
+    private final int numberOfWinners;
+    private final GameResult gameResult;
+    public WinningResult(int numberOfWinners, GameResult gameResult) {
+        this.numberOfWinners = numberOfWinners;
+        this.gameResult = gameResult;
+    }
+    public int getNumberOfWinners() {
+        return numberOfWinners;
+    }
+    public GameResult getGameResult() {
+        return gameResult;
+    }
+}
