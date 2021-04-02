2e761cf60c8a99ebc53f7310b642f72f9629e4cc
Chae hee chan <eqholic1125@gmail.com>
2021-04-02 23:45:20 +0900

    refactor: LottoTicket 구조 변경

---
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+class LottoTicketTest {
+    @Test
+    void numberOfLottoNumbers() {
+        //given
+        int expected = 6;
+        //when
+        LottoTicket lottoTicket = new LottoTicket();
+        List<LottoNumber> lottoNumbers = lottoTicket.getLottoNumbers();
+        int actual = lottoNumbers.size();
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void rangeOfLottoNumbers() {
+        //given
+        final int UPPER_BOUND = 45;
+        final int LOWER_BOUND = 1;
+        //when
+        LottoTicket lottoTicket = new LottoTicket();
+        List<LottoNumber> lottoNumbers = lottoTicket.getLottoNumbers();
+        //then
+        lottoNumbers.forEach(
+                lottoNumber -> assertThat(lottoNumber.getLottoNumber())
+                        .isGreaterThanOrEqualTo(LOWER_BOUND)
+                        .isLessThanOrEqualTo(UPPER_BOUND)
+        );
+    }
+}
