a87480f58c89af893d2e180aeb2714495c1dffe6
Chae hee chan <eqholic1125@gmail.com>
2021-04-02 23:55:33 +0900

    test: WinningResultTest작성

---
+++ src/test/java/lotto/domain/WinningResultTest.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinningResultTest {
+
+    @Test
+    void getNumberOfWinners() {
+    }
+}
