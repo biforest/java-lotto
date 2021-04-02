f9638c71c6162d80cb85edb7481db8c29adb090d
Chae hee chan <eqholic1125@gmail.com>
2021-04-02 23:54:32 +0900

    test: MatchCountTest 작성

---
+++ src/test/java/lotto/domain/MatchCountTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchCountTest {
+
+    @Test
+    void isUnderThree() {
+    }
+
+    @Test
+    void isMatchThree() {
+    }
+
+    @Test
+    void isMatchFour() {
+    }
+
+    @Test
+    void isMatchFiveWithoutBonus() {
+    }
+
+    @Test
+    void isMatchFiveWithBonus() {
+    }
+
+    @Test
+    void isAllMatch() {
+    }
+}
