f13151d3ef6f5f07a30cfed73990718e96204190
Chae hee chan <eqholic1125@gmail.com>
2021-04-02 23:53:07 +0900

    feat: GameResults 구현

---
+++ src/main/java/lotto/domain/GameResults.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+import java.util.ArrayList;
+import java.util.List;
+public class GameResults {
+    private static final double NO_PRIZE = 0;
+    private final List<GameResult> results;
+    public GameResults(List<GameResult> results) {
+        this.results = new ArrayList<>(results);
+    }
+    public double calculatePrize() {
+        return results.stream()
+                .map(GameResult::getPrize)
+                .mapToDouble(Double::valueOf)
+                .reduce(Double::sum)
+                .orElse(NO_PRIZE);
+    }
+    public int count(GameResult gameResult) {
+        return Long.valueOf(
+                results.stream()
+                        .filter(gameResult::equals)
+                        .count()
+        ).intValue();
+    }
+}
