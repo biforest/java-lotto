bd640a74e1195561a6975b1e9c37634cd50a2a2b
pkalsh <pkalsh345@gmail.com>
2021-04-02 13:53:48 +0900

    refactor: 당첨 번호 제약 조건 추가

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class WinningNumber {
+    private static final String BONUS_NUMBER_EXCEPTION_MESSAGE = "보너스 번호는 당첨 번호와 다른 번호여야 합니다.";
 
     private final List<LottoNumber> winningNumbers;
     private final LottoNumber bonusNumber;
 
     public WinningNumber(List<Integer> winningNumbers, int bonusNumber) {
+        validate(winningNumbers, bonusNumber);
+
         this.winningNumbers = winningNumbers.stream()
                 .map(LottoNumber::new)
                 .collect(Collectors.toUnmodifiableList());
@@ -16,4 +19,9 @@ public class WinningNumber {
         this.bonusNumber = new LottoNumber(bonusNumber);
     }
 
+    private void validate(List<Integer> winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_EXCEPTION_MESSAGE);
+        }
+    }
 }
