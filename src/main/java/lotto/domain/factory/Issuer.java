fb0c87f27eaf63455daed140850f86ba827d619e
pkalsh <pkalsh345@gmail.com>
2021-04-04 17:28:58 +0900

    refactor: IntStream을 이용하면 int 값만큼 객체 생성하는 명령을 함수 표현식으로 가능

--- src/main/java/lotto/domain/factory/Issuer.java
+++ src/main/java/lotto/domain/factory/Issuer.java
@@ -3,8 +3,8 @@ package lotto.domain.factory;
 import lotto.domain.lotto.LottoTicket;
 import lotto.domain.lotto.LottoTickets;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Issuer {
     private static final int TICKET_PRICE = 1000;
@@ -16,11 +16,11 @@ public class Issuer {
         int numberOfTickets = money / TICKET_PRICE;
         validate(numberOfTickets);
 
-        List<LottoTicket> lottoTickets = new ArrayList<>();
-        for (int i = 0; i < numberOfTickets; i++) {
-            lottoTickets.add(new LottoTicket());
-        }
-        return new LottoTickets(lottoTickets);
+        return new LottoTickets(
+                IntStream.range(0, numberOfTickets)
+                        .mapToObj(__ -> new LottoTicket())
+                        .collect(Collectors.toList())
+        );
     }
 
     private static void validate(int numberOfTickets) {
