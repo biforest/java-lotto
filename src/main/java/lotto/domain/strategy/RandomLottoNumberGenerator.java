c162ffd10cf58f4b303a83285c448ce23f3c4d07
Sehoon In <dlstpgns0406@gmail.com>
2021-04-09 04:43:15 +0900

    refactor: list 에 담기지 않는 문제로 인해 기존 로직으로 수정

--- src/main/java/lotto/domain/strategy/RandomLottoNumberGenerator.java
+++ src/main/java/lotto/domain/strategy/RandomLottoNumberGenerator.java
@@ -9,26 +9,20 @@ public class RandomLottoNumberGenerator {
     private final static int LOWER_BOUND = 1;
     private final static int LOTTO_NUMBER_COUNT = 6;
 
-    private final List<Integer> lottoNumberRange;
-
-    public RandomLottoNumberGenerator() {
-        lottoNumberRange = new ArrayList<>();
-        setLottoNumberRange();
-    }
-
-    public List<Integer> getRandomLottoNumbers(){
-        return getLottoNumbers(lottoNumberRange);
-    }
-
-    private void setLottoNumberRange(){
+    private List<Integer> setLottoNumberRange(){
+        List<Integer> lottoNumberRange = new ArrayList<>();
         for (int lottoNumber = LOWER_BOUND; lottoNumber < UPPER_BOUND; lottoNumber++) {
             lottoNumberRange.add(lottoNumber);
         }
+        return lottoNumberRange;
     }
 
-    private List<Integer> getLottoNumbers(List<Integer> lottoNumberRange){
+    public List<Integer> getRandomLottoNumbers(){
+        List<Integer> lottoNumberRange = setLottoNumberRange();
         Collections.shuffle(lottoNumberRange);
-        return lottoNumberRange.subList(0,LOTTO_NUMBER_COUNT);
+        List<Integer> collectedNumbers = lottoNumberRange.subList(0,LOTTO_NUMBER_COUNT);
+        Collections.sort(collectedNumbers);
+        return collectedNumbers;
     }
 
 }
