b93c4555ebfb65bb224cb39d43b19d14fcb31e43
Sehoon In <dlstpgns0406@gmail.com>
2021-04-08 18:46:05 +0900

    feat: NotValidLottoTicketOfNumberException class 추가

---
+++ src/main/java/lotto/domain/exception/NotValidLottoTicketOfNumberException.java
@@ -0,0 +1,9 @@
+package lotto.domain.exception;
+
+public class NotValidLottoTicketOfNumberException extends RuntimeException{
+    private static final String MESSAGE = "로또를 살 금액이 부족합니다.";
+
+    public NotValidLottoTicketOfNumberException(){
+        super(MESSAGE);
+    }
+}
