ff36d2f17ebe29be4a5b4fe6c828e75073a27c18
Sehoon In <dlstpgns0406@gmail.com>
2021-04-08 18:48:40 +0900

    refactor: LottoManualTicket(수동복권) class 추가

---
+++ src/main/java/lotto/domain/lotto/LottoManualTicket.java
@@ -0,0 +1,16 @@
+package lotto.domain.lotto;
+
+import java.util.List;
+
+public class LottoManualTicket extends Lotto{
+    private final List<Integer> numbers;
+
+    public LottoManualTicket(List<Integer> numbers) {
+        super(numbers);
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getLotto(){
+        return numbers;
+    }
+}
