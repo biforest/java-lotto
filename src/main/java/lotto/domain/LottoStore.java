26d61ed3ef1916b8e123ed46c5a2b93c54f2b45d
Sehoon In <dlstpgns0406@gmail.com>
2021-04-09 01:12:41 +0900

    feat: lottoManualTicket 객체들을 만들고 lottoManualTickets 객체를 만드는 inputLottoManualTickets method 구현

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -1,7 +1,11 @@
 package lotto.domain;
 
+import lotto.domain.lotto.LottoManualTicket;
+import lotto.domain.lotto.LottoManualTickets;
 import lotto.ui.Printer;
 import lotto.ui.Receiver;
+
+import java.util.ArrayList;
 
 public class LottoStore {
     private final Printer printer = new Printer();
@@ -17,5 +21,16 @@ public class LottoStore {
     public int inputLottoManualTicketNumber() {
         printer.requestLottoManualTicketNumber();
         return receiver.receiverLottoManualTicketNumber();
+    }
+
+    public LottoManualTickets inputLottoManualTickets(int lottoManualTicketNumber) {
+        ArrayList<LottoManualTicket> lottoManualTicketDummy = new ArrayList<>();
+        printer.requestLottoManualTicket();
+        for (int lottoTicket = 0; lottoTicket < lottoManualTicketNumber; lottoTicket++) {
+            LottoManualTicket lottoManualTicket = new LottoManualTicket(receiver.receiverLottoManualTicket());
+            lottoManualTicketDummy.add(lottoManualTicket);
+        }
+        LottoManualTickets lottoManualTickets = new LottoManualTickets(lottoManualTicketDummy);
+        return lottoManualTickets;
     }
 }
