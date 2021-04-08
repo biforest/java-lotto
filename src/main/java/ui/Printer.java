b395cebd087767c54a136b55d0399d87ed0fceb5
songpang <scc6920@naver.com>
2021-04-08 22:38:03 +0900

    feat: 기능 변경에 따른 입/출력 클래스 수정

--- src/main/java/ui/Printer.java
+++ src/main/java/ui/Printer.java
@@ -1,6 +1,7 @@
 package ui;
 
 import java.util.List;
+import java.util.Map;
 
 import domain.lotteryStore.Lotteries;
 import domain.winningStatistics.PrizeMoney;
@@ -8,30 +9,28 @@ import domain.winningStatistics.WinningStatistics;
 import ui.message.OutputMessage;
 
 public class Printer {
-    private static final int SIZE_OF_LOTTERY_RANK = 5;
+    private static final int SIZE_OF_RANK = 5;
     private static final int WIN_WITH_BONUS_NUMBER = 3;
-    private static final String LIMIT_OF_DECIMAL_PLACE = "%.2f";
 
     public void printPurchasedCount(int manualCount, int purchasedCount) {
-        System.out.println(
-            String.format(OutputMessage.PURCHASED_COUNT.getMessage(), manualCount, purchasedCount - manualCount));
+        System.out.printf(OutputMessage.PURCHASED_COUNT.getMessage(), manualCount, purchasedCount - manualCount);
     }
 
     public void printPurchasedLotteries(Lotteries lotteries) {
         lotteries.getLotteries()
-            .stream()
-            .map(lottery -> lottery.getNumbers().getNumbers())
-            .forEach(System.out::println);
+                .stream()
+                .map(lottery -> lottery.getNumbers().getNumbers())
+                .forEach(System.out::println);
     }
 
     public void printWinningStatistics(WinningStatistics winningStatistics) {
         StringBuilder builder = new StringBuilder();
         PrizeMoney[] prizeMonies = PrizeMoney.values();
-        List<Integer> sizeOfWinnersPerRank = winningStatistics.getRankings();
 
         builder.append(OutputMessage.WINNING_STATISTICS.getMessage());
-        for (int i = 0; i < SIZE_OF_LOTTERY_RANK; i++) {
-            printWinningStatisticsDetails(builder, prizeMonies[i], sizeOfWinnersPerRank.get(i), i);
+
+        for (int i = 0; i < SIZE_OF_RANK; i++) {
+            printWinningStatisticsDetails(builder, prizeMonies[i], winningStatistics.get(prizeMonies[i]), i);
         }
         System.out.print(builder);
     }
@@ -39,16 +38,15 @@ public class Printer {
     private void printWinningStatisticsDetails(StringBuilder builder, PrizeMoney prizeMoney, Integer result, int i) {
         if (i == WIN_WITH_BONUS_NUMBER) {
             builder.append(
-                String.format(OutputMessage.MATCH_COUNT_WITH_BONUS.getMessage(), prizeMoney.getMatchingCount(),
-                    prizeMoney.getPrizeMoney(), result));
+                    String.format(OutputMessage.MATCH_COUNT_WITH_BONUS.getMessage(), prizeMoney.getMatchingCount(),
+                            prizeMoney.getPrizeMoney(), result));
+            return;
         }
-        if (i != WIN_WITH_BONUS_NUMBER) {
-            builder.append(String.format(OutputMessage.MATCH_COUNT.getMessage(), prizeMoney.getMatchingCount(),
+        builder.append(String.format(OutputMessage.MATCH_COUNT.getMessage(), prizeMoney.getMatchingCount(),
                 prizeMoney.getPrizeMoney(), result));
-        }
     }
 
     public void printTotalEarningsRate(float totalEarningsRate) {
-        System.out.println(String.format(OutputMessage.TOTAL_EARNINGS_RATE.getMessage(), totalEarningsRate));
+        System.out.printf(OutputMessage.TOTAL_EARNINGS_RATE.getMessage(), totalEarningsRate);
     }
 }
