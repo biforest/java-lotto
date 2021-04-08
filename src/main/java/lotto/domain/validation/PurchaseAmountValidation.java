626d3ce0f0a51825629da74b0dbc5ba8771c2422
Sehoon In <dlstpgns0406@gmail.com>
2021-04-08 16:55:47 +0900

    feat: PurchaseAmount 객체에 대한 validation 체크를 해주는 class 생성

---
+++ src/main/java/lotto/domain/validation/PurchaseAmountValidation.java
@@ -0,0 +1,12 @@
+package lotto.domain.validation;
+
+public class PurchaseAmountValidation {
+    private static final int MIN_PURCHASE_AMOUNT = 0;
+
+    public void checkPurchaseAmountNotPositive(int purchaseAmount) {
+        if (purchaseAmount <= MIN_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("구매 금액은 0원 이하가 될 수 없습니다.");
+        }
+    }
+
+}
