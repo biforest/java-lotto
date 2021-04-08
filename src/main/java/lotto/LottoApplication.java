da70a37dfed842e87921c25213cbaadc6b87a858
Sehoon In <dlstpgns0406@gmail.com>
2021-04-08 23:55:57 +0900

    feat: LottoApplication class 생성

---
+++ src/main/java/lotto/LottoApplication.java
@@ -0,0 +1,13 @@
+package lotto;
+
+public class LottoApplication {
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        LottoApplication app = new LottoApplication();
+        app.run();
+    }
+}
