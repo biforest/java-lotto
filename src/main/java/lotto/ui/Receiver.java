d7e838b3c7baeef7869fd3d82be59f7f455b4ea9
Sehoon In <dlstpgns0406@gmail.com>
2021-04-09 00:50:05 +0900

    feat: receiverLottoManualTickets method 구현 및 method 에 필요한 convertStringToInt method 와 splitInputDelimiter method 구현

--- src/main/java/lotto/ui/Receiver.java
+++ src/main/java/lotto/ui/Receiver.java
@@ -1,13 +1,36 @@
 package lotto.ui;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Receiver {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String DELIMITER = ", ";
 
     public int receiveLottoPurchaseAmount() {
         return scanner.nextInt();
     }
 
-    public int receiverLottoManualTicketNumber() {return scanner.nextInt();}
+    public int receiverLottoManualTicketNumber() {
+        return scanner.nextInt();
+    }
+
+    public List<Integer> receiverLottoManualTickets() {
+        String lottoTicket = scanner.next();
+        List<String> lottoManualTicket = splitInputDelimiter(lottoTicket);
+        return convertStringToInt(lottoManualTicket);
+    }
+
+    private List<Integer> convertStringToInt(List<String> lottoManualTicket) {
+        return lottoManualTicket.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> splitInputDelimiter(String lottoManualTicket) {
+        return Arrays.asList(lottoManualTicket.split(DELIMITER));
+    }
 }
