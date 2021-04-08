7b6046af9f2c424015a0a33e4321ae5360a257a8
songpang <scc6920@naver.com>
2021-04-08 22:36:32 +0900

    refactor: PrizeMoney 클래스 내 값 관리 List에서 Map으로 변경

--- src/main/java/domain/winningStatistics/PrizeMoney.java
+++ src/main/java/domain/winningStatistics/PrizeMoney.java
@@ -1,34 +1,39 @@
 package domain.winningStatistics;
 
+import java.util.Arrays;
 import java.util.List;
 
 public enum PrizeMoney {
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    FIVE_BONUS(5, 30000000),
-    SIX(6, 2000000000);
+    THREE(3, false, 5000),
+    FOUR(4, false, 50000),
+    FIVE(5, false, 1500000),
+    FIVE_BONUS(5, true, 30000000),
+    SIX(6, false, 2000000000);
 
     private final int matchingCount;
+    private final boolean isHavingBonusNumber;
     private final int prizeMoney;
 
-    PrizeMoney(int matchingCount, int prizeMoney) {
+    PrizeMoney(int matchingCount, boolean isHavingBonusNumber, int prizeMoney) {
         this.matchingCount = matchingCount;
+        this.isHavingBonusNumber = isHavingBonusNumber;
         this.prizeMoney = prizeMoney;
     }
 
     public static float calculateEarningsRate(WinningStatistics winningStatistics, int purchasedCount) {
-        List<Integer> sizeOfWinnersPerRank = winningStatistics.getRankings();
-        int totalPrizeMoney = 0;
-        for (int i = 0; i < sizeOfWinnersPerRank.size(); i++) {
-            totalPrizeMoney += sizeOfWinnersPerRank.get(i) * (PrizeMoney.values())[i].prizeMoney;
-        }
+        int totalPrizeMoney = Arrays.stream(values())
+                .mapToInt(prizeMoney -> winningStatistics.get(prizeMoney) * prizeMoney.getPrizeMoney())
+                .sum();
 
         return (float)totalPrizeMoney / (purchasedCount * 1000);
     }
 
     public int getMatchingCount() {
         return matchingCount;
+    }
+
+    public boolean isHavingBonusNumber() {
+        return isHavingBonusNumber;
     }
 
     public int getPrizeMoney() {
