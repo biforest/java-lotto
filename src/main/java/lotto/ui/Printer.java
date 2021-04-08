31a24f1e3678ab51a801eff62f1ebcb5a5798aa1
Sehoon In <dlstpgns0406@gmail.com>
2021-04-09 00:50:39 +0900

    feat: requestLottoManualTicket method 구현

--- src/main/java/lotto/ui/Printer.java
+++ src/main/java/lotto/ui/Printer.java
@@ -3,6 +3,7 @@ package lotto.ui;
 public class Printer {
     private static final String REQUEST_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String REQUEST_LOTTO_MANUAL_TICKET_NUMBER_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
+    private static final String REQUEST_LOTTO_MANUAL_TICKET_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
 
     public void requestPurchaseAmount() {
         System.out.println(REQUEST_PURCHASE_AMOUNT_MESSAGE);
@@ -11,4 +12,9 @@ public class Printer {
     public void requestLottoManualTicketNumber() {
         System.out.println(REQUEST_LOTTO_MANUAL_TICKET_NUMBER_MESSAGE);
     }
+
+    public void requestLottoManualTicket() {
+        System.out.println(REQUEST_LOTTO_MANUAL_TICKET_MESSAGE);
+    }
+
 }
