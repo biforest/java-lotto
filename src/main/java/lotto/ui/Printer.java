bd9ad045286fe06cac3e53fc07bc1bd9f4675d4d
Sehoon In <dlstpgns0406@gmail.com>
2021-04-09 04:42:44 +0900

    feat: requestLastWeekLottoWinningNumber method 구현

--- src/main/java/lotto/ui/Printer.java
+++ src/main/java/lotto/ui/Printer.java
@@ -11,6 +11,8 @@ public class Printer {
     private static final String REQUEST_LOTTO_MANUAL_TICKET_NUMBER_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String REQUEST_LOTTO_MANUAL_TICKET_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String PRINT_NUMBER_OF_LOTTO_TICKET_MESSAGE = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
+    private static final String REQUEST_LAST_WEEK_LOTTO_WINNING_NUMBER_MESSAGE = "\n지난 주 당첨 번호를 입력해 주세요.";
+
 
     public void requestPurchaseAmount() {
         System.out.println(REQUEST_PURCHASE_AMOUNT_MESSAGE);
@@ -42,5 +44,9 @@ public class Printer {
         for (LottoManualTicket lottoManualTicket: lottoManualTickets.getLottoManualTickets()) {
             System.out.println(lottoManualTicket.getLotto());
         }
+    }
+
+    public void requestLastWeekLottoWinningNumber() {
+        System.out.println(REQUEST_LAST_WEEK_LOTTO_WINNING_NUMBER_MESSAGE);
     }
 }
