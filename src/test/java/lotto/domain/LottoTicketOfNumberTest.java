105b98226094a0989b5de4182fb185d3d0685f37
Sehoon In <dlstpgns0406@gmail.com>
2021-04-08 18:45:15 +0900

    feat: 자동 로또 티켓의 수를 가져오는 test case 와 입력할 로또 구입 금액으로 로또를 살 수 없는 경우 NotValidLottoTicketOfNumberException 를 던지는 test case 작성

--- src/test/java/lotto/domain/LottoTicketOfNumberTest.java
+++ src/test/java/lotto/domain/LottoTicketOfNumberTest.java
@@ -1,16 +1,19 @@
 package lotto.domain;
 
+import lotto.domain.exception.NotValidLottoTicketOfNumberException;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class LottoTicketOfNumberTest {
     @Test
-    void 로또_티켓_수를_관리하는_객체를_생성한다() {
+    void 로또_티켓의_총_수를_관리하는_객체를_생성한다() {
         //given
         int purchaseAmountNumber = 11000;
+        int manualLottoTicketOfNumber = 3;
         int actual = 11;
-        LottoTicketOfNumber lottoTicketOfNumber = new LottoTicketOfNumber(purchaseAmountNumber);
+        LottoTicketOfNumber lottoTicketOfNumber = new LottoTicketOfNumber(purchaseAmountNumber, manualLottoTicketOfNumber);
 
         //when
         int expected = lottoTicketOfNumber.getLottoTicketOfNumber();
@@ -18,5 +21,32 @@ class LottoTicketOfNumberTest {
 
         //then
         assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    void 자동_로또_티켓의_수를_가져온다() {
+        //given
+        int purchaseAmountNumber = 11000;
+        int manualLottoTicketOfNumber = 3;
+        int actual = 8;
+        LottoTicketOfNumber lottoTicketOfNumber = new LottoTicketOfNumber(purchaseAmountNumber, manualLottoTicketOfNumber);
+
+        //when
+        int expected = lottoTicketOfNumber.getAutomaticLottoTicketOfNumber();
+        System.out.println(expected);
+
+        //then
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    void 입력한_로또_구입_금액으로_로또를_살_수_없는_경우_NotValidLottoTicketOfNumberException_을_던진다() {
+        //given
+        int purchaseAmount = 900;
+        int manualLottoTicketOfNumber = 3;
+        //when
+        //then
+        assertThatExceptionOfType(NotValidLottoTicketOfNumberException.class).
+                isThrownBy(() -> new LottoTicketOfNumber(purchaseAmount, manualLottoTicketOfNumber));
     }
 }
