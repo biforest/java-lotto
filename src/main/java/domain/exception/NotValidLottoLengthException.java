37f86afb351d376746863457431b1e551c315b29
ichangyu <lcg6307@naver.com>
2021-04-09 00:14:02 +0900

    refactor: exception 삭제

--- src/main/java/domain/exception/NotValidLottoLengthException.java
+++
@@ -1,9 +0,0 @@
-package domain.exception;
-
-public class NotValidLottoLengthException extends RuntimeException {
-    private static final String MESSAGE = "번호는 6개를 입력해야 합니다.";
-
-    public NotValidLottoLengthException() {
-        super(MESSAGE);
-    }
-}
