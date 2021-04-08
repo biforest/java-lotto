a159daa0e56384297ca2f44fd145047f376d4674
Sehoon In <dlstpgns0406@gmail.com>
2021-04-08 16:15:06 +0900

    feat: checkDuplicatedNumber method 구현

--- src/main/java/lotto/domain/validation/LottoValidation.java
+++ src/main/java/lotto/domain/validation/LottoValidation.java
@@ -1,15 +1,31 @@
 package lotto.domain.validation;
 
+import lotto.domain.exception.NotDuplicatedNumberException;
 import lotto.domain.exception.NotValidLottoLengthException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoValidation {
     private final static int LOTTO_NUMBER_COUNT = 6;
 
-    public void checkLottoLength(List<Integer> numbers){
+    public void checkLottoLength(List<Integer> numbers) {
         if(numbers.size() !=LOTTO_NUMBER_COUNT){
             throw new NotValidLottoLengthException();
         }
+    }
+
+    public void checkDuplicatedNumber(List<Integer> numbers) {
+        List<Integer> duplicatedCheckNumbers = new ArrayList<>();
+        for (Integer number :numbers) {
+            hasDuplicatedNumber(number, duplicatedCheckNumbers);
+        }
+    }
+
+    private void hasDuplicatedNumber(Integer number, List<Integer> numbers) {
+        if(numbers.contains(number)) {
+            throw new NotDuplicatedNumberException();
+        }
+        numbers.add(number);
     }
 }
