e85095501090178394cfa432085c3a77498ff893
Sehoon In <dlstpgns0406@gmail.com>
2021-04-09 04:53:18 +0900

    feat: 보너스볼을 입력을 요구하는 메시지 출력 담당의 requestLottoBonusBallNumber method 구현

--- src/main/java/lotto/ui/Printer.java
+++ src/main/java/lotto/ui/Printer.java
@@ -12,7 +12,7 @@ public class Printer {
     private static final String REQUEST_LOTTO_MANUAL_TICKET_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
     private static final String PRINT_NUMBER_OF_LOTTO_TICKET_MESSAGE = "수동으로 %d장, 자동으로 %d개를 구매했습니다.";
     private static final String REQUEST_LAST_WEEK_LOTTO_WINNING_NUMBER_MESSAGE = "\n지난 주 당첨 번호를 입력해 주세요.";
-
+    private static final String REQUEST_LOTTO_BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해주세요";
 
     public void requestPurchaseAmount() {
         System.out.println(REQUEST_PURCHASE_AMOUNT_MESSAGE);
@@ -48,5 +48,9 @@ public class Printer {
 
     public void requestLastWeekLottoWinningNumber() {
         System.out.println(REQUEST_LAST_WEEK_LOTTO_WINNING_NUMBER_MESSAGE);
+    }
+
+    public void requestLottoBonusBallNumber() {
+        System.out.println(REQUEST_LOTTO_BONUS_BALL_NUMBER_MESSAGE);
     }
 }
