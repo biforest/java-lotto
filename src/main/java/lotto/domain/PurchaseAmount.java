8eca2f21ed2080989fd389f5ef92d19385db417a
oereo <dlstpgns0406@gmail.com>
2021-04-10 05:05:18 +0900

    refactor: checkPurchaseAmountNotPositive method 를 PurchaseAmount class 에서 체크할 수 있도록 구조 변경

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,14 +1,20 @@
 package lotto.domain;
 
-import lotto.domain.validation.PurchaseAmountValidation;
 
 public class PurchaseAmount {
+    private static final int MIN_PURCHASE_AMOUNT = 0;
+
     private final int purchaseAmount;
 
     public PurchaseAmount(int purchaseAmount) {
-        PurchaseAmountValidation purchaseAmountValidation = new PurchaseAmountValidation();
         this.purchaseAmount = purchaseAmount;
-        purchaseAmountValidation.checkPurchaseAmountNotPositive(this.purchaseAmount);
+        checkPurchaseAmountNotPositive(purchaseAmount);
+    }
+
+    private void checkPurchaseAmountNotPositive(int purchaseAmount) {
+        if (purchaseAmount <= MIN_PURCHASE_AMOUNT) {
+            throw new IllegalArgumentException("구매 금액은 0원 이하가 될 수 없습니다.");
+        }
     }
 
     public int getPurchaseAmount() {
