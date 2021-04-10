5fc9d720f71a46397b87ac994f0b75a8db3fc038
pkalsh <pkalsh345@gmail.com>
2021-04-11 04:08:23 +0900

    refactor: LottoNumbers로 리팩토링함에 따라 List로 갖고 있던 객체들의 속성 또한 리팩토링
    - LottoNumbers에서 중복된 로또 번호 및 로또 번호 개수 검증

--- src/main/java/lotto/domain/lotto/LottoTicket.java
+++ src/main/java/lotto/domain/lotto/LottoTicket.java
@@ -1,37 +1,18 @@
 package lotto.domain.lotto;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class LottoTicket {
-    private static final int NUMBER_OF_LOTTO_NUMBERS = 6;
 
-    private final List<LottoNumber> lottoNumbers;
+    private final LottoNumbers lottoNumbers;
 
-    public LottoTicket() {
-        List<LottoNumber> lottoNumbers = LottoNumber.range();
-        lottoNumbers = generateLottoNumbers(lottoNumbers);
-        this.lottoNumbers = new ArrayList<>(lottoNumbers);
-    }
-
-    private List<LottoNumber> generateLottoNumbers(List<LottoNumber> lottoNumbers) {
-        Collections.shuffle(lottoNumbers);
-        lottoNumbers = lottoNumbers.stream()
-                .limit(NUMBER_OF_LOTTO_NUMBERS)
-                .sorted(Comparator.comparing(LottoNumber::getLottoNumber))
-                .collect(Collectors.toList());
-
-        return lottoNumbers;
+    public LottoTicket(LottoNumbers lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
     }
 
     public boolean contains(LottoNumber lottoNumber) {
         return lottoNumbers.contains(lottoNumber);
     }
 
-    public List<LottoNumber> getLottoNumbers() {
+    public LottoNumbers getLottoNumbers() {
         return lottoNumbers;
     }
 }
--- src/main/java/lotto/domain/lotto/WinningNumber.java
+++ src/main/java/lotto/domain/lotto/WinningNumber.java
@@ -8,15 +8,17 @@ import java.util.stream.Collectors;
 public class WinningNumber {
     private static final String BONUS_NUMBER_EXCEPTION_MESSAGE = "보너스 번호는 당첨 번호와 다른 번호여야 합니다.";
 
-    private final List<LottoNumber> winningNumbers;
+    private final LottoNumbers winningNumbers;
     private final LottoNumber bonusNumber;
 
     public WinningNumber(List<Integer> winningNumbers, int bonusNumber) {
         validate(winningNumbers, bonusNumber);
 
-        this.winningNumbers = winningNumbers.stream()
+        this.winningNumbers = new LottoNumbers(
+                winningNumbers.stream()
                 .map(LottoNumber::new)
-                .collect(Collectors.toUnmodifiableList());
+                .collect(Collectors.toUnmodifiableList())
+        );
 
         this.bonusNumber = new LottoNumber(bonusNumber);
     }
@@ -29,7 +31,8 @@ public class WinningNumber {
 
     public MatchCount matchNumbers(LottoTicket lottoTicket) {
         int matchCount = Long.valueOf(
-                winningNumbers.stream()
+                winningNumbers.getLottoNumbers()
+                        .stream()
                         .filter(lottoTicket::contains)
                         .count()
         ).intValue();
